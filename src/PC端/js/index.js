e9cdd2fbbe660c9902453483313b5b6862572e94
半只兔子 <13772608541@163.com>
2022-05-24 18:30:16 +0800

    修复下拉框bug

--- src/PC端/js/index.js
+++ src/PC端/js/index.js
@@ -8,7 +8,8 @@ $(function() {
         if ($(this).hasClass('active')) {
             $(this).removeClass('active')
         } else {
-            $(this).addClass('active').siblings().removeClass('active')
+            $('.selects div').removeClass('active')
+            $(this).addClass('active')
         }
         $('.selects ul').stop().slideUp()
         $('.selects .active ul').stop().slideDown()
